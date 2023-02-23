aab370ae993e1cd0bafcc9bcaa9522bee91ba29b
bb0lshak0vv <58132397+bb0lshak0vv@users.noreply.github.com>
2023-02-23 14:41:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,20 @@
 
 
 bool checkPrime(uint64_t value) {
-  for (uint16_t i = 2; i * i < value; i++)
-          if (value % i == 0)
-              return false;
-      return true;
+    for (uint64_t i = 2; i * i < value; i++){
+            if (value % i == 0){
+                return false;
+            }
+    }
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  uint64_t res = 1, cnt = 0;
-  while (cnt < n){
+  uint64_t res = 1; 
+  uint64_t cnt = 0;
+  while (cnt != n){
     res++;
-    if (checkprime(res))
+    if (checkPrime(res) == true)
       cnt++;
   }
   return res;
@@ -24,17 +27,18 @@ uint64_t nextPrime(uint64_t value) {
   bool checker = false;
   while (checker == false){
     value++;
-    checker = checkprime(value);
+    checker = checkPrime(value);
   }
   return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
   uint64_t sum = 0;
-  for (int i = 2; i < hbound; i++){
-    if (checkprime(i)) {
-      sum += i;
-    }
+  uint64_t res = 2;
+  while (result != hbound) {
+    if (checkPrime(res) == true)
+      sum += res;
+    res++;
   }
   return sum;
 }
